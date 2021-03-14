0a8e3cdeba45016cd8aca410380842b58139e4f6
Fabian Romero <fabian.romero.jimenez@gmail.com>
2021-03-14 15:19:08 -0500

    Get Products User

--- src/app/components/user-products/user-products.component.ts
+++ src/app/components/user-products/user-products.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductClientService } from 'src/app/services/productClient.service';
 import { AuthService } from 'src/app/services/auth.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouteConstants } from 'src/app/utils/route-constants';
 
 @Component({
   selector: 'app-user-products',
@@ -13,7 +14,8 @@ export class UserProductsComponent implements OnInit {
   listProducts = [];
   id;
 
-  constructor(private productsClientService: ProductClientService, private activatedRoute: ActivatedRoute) {
+  constructor(private productsClientService: ProductClientService, private activatedRoute: ActivatedRoute,
+    private router: Router) {
     this.activatedRoute.queryParams.subscribe(params => {
       this.id = params['id'];
       this.getListProducts();
@@ -30,5 +32,8 @@ export class UserProductsComponent implements OnInit {
         element.value = element.cantidad * element.precio;
       });
     })
+  }
+  navigateToHome(){
+    this.router.navigate([RouteConstants.PRODUCT_LIST_PATH]);
   }
 }
